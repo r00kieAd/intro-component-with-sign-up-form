2ae0b57efecc9d40190764222d6a6d82db04ec6b
Adhyatma Dwivedi <113773621+r00kieAd@users.noreply.github.com>
2024-06-05 23:05:03 +0530

    code complete

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $('.inp input').focus(function() {
             "color": "hsl(249, 10%, 26%)"
         }
     );
+    $('.err-ico').fadeOut();
     $(`.${$(this).prop('name')} span`).hide();
 })
 
@@ -13,7 +14,8 @@ $('.inp input').blur(function() {
     $(`.${$(this).prop('name')} span`).hide();
 })
 
-function showErr(e) {
+function showErr(e, i) {
+    $('.err-ico').eq(i).fadeIn();
     $(e).css(
         {
             "border-color": "hsl(0, 100%, 74%)",
@@ -26,26 +28,38 @@ $('.sub button').click(
     function() {
         const inputs = document.querySelectorAll('.inp input');
         i = 1;
+        hasErr = false;
         inputs.forEach((e) => {
             const text = e.value;
             if (text == "") {
                 $(`.e${i}`).show();
-                showErr($('.inp input').eq(i-1));
+                showErr($('.inp input').eq(i-1), i-1);
+                hasErr = true;
             } else {
                 if (i <= 2) {
                     if (!/^[a-zA-Z]+$/.test(text)) {
                         $(`.invalid${i}`).show();
-                        showErr($('.inp input').eq(i-1)); 
+                        showErr($('.inp input').eq(i-1), i-1); 
+                        hasErr = true;
                     }
-                }
-                if (i == 3) {
+                } else if (i == 3) {
                     if (!/^\S+@\S+\.\S+$/.test(text)) {
                         $(`.e${i}`).show();
-                        showErr($('.inp input').eq(i-1)); 
+                        showErr($('.inp input').eq(i-1), i-1); 
+                        hasErr = true;
+                    }
+                } else {
+                    if (text.length < 8) {
+                        $(`.e${i}`).show();
+                        showErr($('.inp input').eq(i-1), i-1); 
+                        hasErr = true;
                     }
                 }
             }
             i++;
-        })
+        });
+        if (!hasErr) {
+            alert('Request Received!');
+        }
     }
 )
